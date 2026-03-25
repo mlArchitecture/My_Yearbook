e90b2147ca8312369e2e0304ccaf1731c9ab4b68
Rupesh Sahoo <146566370+mlArchitecture@users.noreply.github.com>
2026-03-25 23:38:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,6 @@ app.get("/image", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
